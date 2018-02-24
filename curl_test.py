b670c8c23a653d512afcb83a7f33eb08b296548b
Stephen Thomas <mewzyk@gmail.com>
2018-02-23 23:37:12 -0800

    First commit

--- curl_test.py
+++ curl_test.py
@@ -3,12 +3,19 @@ import json
 
 r = requests.get('https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=36.5&lon=-121.202&maxDistance=10&minDiff=5.6&maxDiff=5.8&key=200212432-f3aa9be57f04ce46760dce00bef02b2d')
 
-db = r.json()
-climbingRoutes = db['routes']
+database = r.json()
+climbingRoutes = database['routes']
 
 print('Printing Example Data set')
 print('-------------------------')
 
+
+#Loop that prints everything out
+#Climbing routes contains a List of routes
+#Each route is a dictionary with a list of keys
+#This function prints out
+#========================
+# KEY --> route[KEY]
 for route in climbingRoutes:
 	print('==========================')
 	for key in route.keys():
