6e411cae306800467a3d6529c88a181936bfcd18
Ubuntu <ubuntu@ip-172-31-29-3.us-east-2.compute.internal>
2018-02-28 06:17:39 +0000

    Hello

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ def hello():
    r = requests.get('https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=36.5&lon=-121.202&maxDistance=10&minDiff=5.6&maxDiff=5.8&key=200212432-f3aa9be57f04ce46760dce00bef02b2d')
    database = r.json()
    routes = database['routes']
-   print(routes)
 
-   
-   return json.dumps(routes)
+   object_keys = routes.keys()
+
+   return '  '.join(object_keys)
     
 
 if __name__ == '__main__':
