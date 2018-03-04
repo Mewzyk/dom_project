92e2f39063d4e6bf159c4c726ade1d5ea61f352f
bbaltaxe <baltaxeadmony@gmail.com>
2018-03-03 20:32:08 -0800

    Automated commit

---
+++ database/update_db.py
@@ -0,0 +1,16 @@
+import json
+import requests
+
+r = requests.get('https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=36.5&lon=-121.202&maxDistance=10&minDiff=5.6&maxDiff=5.8&key=200212432-f3aa9be57f04ce46760dce00bef02b2d')
+database = r.json()
+routes = database['routes']
+
+d = {}
+for route in routes:
+    
+    d[route['id']] = route
+
+with open('routeInfo.json', 'w') as outfile:
+        json.dump(d, outfile)
+
+
