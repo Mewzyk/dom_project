65228676fb2f1375b3850b3655a652d4d66d63d6
bbaltaxe <baltaxeadmony@gmail.com>
2018-03-03 19:07:02 -0800

    merge conflict resolved

--- app.py
+++ app.py
@@ -14,21 +14,23 @@ def check():
     r = requests.get('https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=36.5&lon=-121.202&maxDistance=10&minDiff=5.6&maxDiff=5.8&key=200212432-f3aa9be57f04ce46760dce00bef02b2d')
     database = r.json()
     routes = database['routes']
-
     object_keys = routes[0].keys()
 
     return '  '.join(object_keys)
 
-
-#   with open('routeInfo.json', 'w') as outfile:
-#           json.dump(routes, outfile)
-
-   object_keys = routes[0].keys()
-
-   return '  '.join(object_keys)
-    
+   
 @app.route('/home') 
 def home():
+
+    
+#    with open('routeInfo.json', 'w') as outfile:
+#            json.dump(routes, outfile)
+
+    object_keys = routes[0].keys()
+
+    return '  '.join(object_keys)
+ 
+    
     return "home!"
 
 if __name__ == '__main__':
