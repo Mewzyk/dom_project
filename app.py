8302eac5948f45874c5e20a8050aeb94609b8dc0
Stephen Thomas <mewzyk@gmail.com>
2018-03-03 20:33:31 -0800

    Hello

--- app.py
+++ app.py
@@ -8,16 +8,6 @@ app = Flask("getRoutey")
 @app.route('/')
 def hello():
     return render_template('index.html')
-
-@app.route('/check_data')
-def check():
-    r = requests.get('https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=36.5&lon=-121.202&maxDistance=10&minDiff=5.6&maxDiff=5.8&key=200212432-f3aa9be57f04ce46760dce00bef02b2d')
-    database = r.json()
-    routes = database['routes']
-    object_keys = routes[0].keys()
-
-    return '  '.join(object_keys)
-
    
 @app.route('/home') 
 def home():
