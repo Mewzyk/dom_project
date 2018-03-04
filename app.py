6a97a704102df1385119e81c29ab9072130e0e11
Stephen Thomas <mewzyk@gmail.com>
2018-03-03 18:59:24 -0800

    Automated commit

--- app.py
+++ app.py
@@ -5,18 +5,20 @@ import json
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
-    
-   r = requests.get('https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=36.5&lon=-121.202&maxDistance=10&minDiff=5.6&maxDiff=5.8&key=200212432-f3aa9be57f04ce46760dce00bef02b2d')
-   database = r.json()
-   routes = database['routes']
+    return render_template('index.html')
 
-   object_keys = routes[0].keys()
+@app.route('/check_data')
+def check():
+    r = requests.get('https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=36.5&lon=-121.202&maxDistance=10&minDiff=5.6&maxDiff=5.8&key=200212432-f3aa9be57f04ce46760dce00bef02b2d')
+    database = r.json()
+    routes = database['routes']
 
-   return '  '.join(object_keys)
-    
+    object_keys = routes[0].keys()
+
+    return '  '.join(object_keys)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
